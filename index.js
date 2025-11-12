0289421f9ec5edacc29b546f0d9f15b124930d4f
bao00022 <bao00022@algonquinlive.com>
2025-11-11 21:40:55 -0500

    Banner part is finished

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
-console.log("Hello World");
+// console.log("Hello World");
+
+const $banner = document.querySelector(".banner");
+const $input = document.querySelector("input");
+
+async function renderBanner(url) {
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+    $banner.innerHTML = `
+      <div class="bannerImg">
+        <img src=${data.image.image.source} alt=${data.image.title}>
+        <div class="starBtn">
+          <img src="./images/star.svg" alt="a star icon">
+        </div>   
+      </div>
+      <div class="bannerContent">
+        <div>
+          <h2>${data.image.title}</h2>
+          <p>${data.mostread.date.slice(0, -1)}</p>
+          </div>                   
+            <p >${data.image.description.text}</p>
+          <div>
+          <button>Read More</button>
+        </div>               
+      </div>             
+    `;
+  } catch (err) {
+    console.error("Error:", err);
+  }
+}
+
+$input.addEventListener("change", (e) => {
+  const date = e.target.value;
+  console.log(date);
+  const replacedDate = date.replaceAll("-", "/");
+  console.log(replacedDate);
+  const url = `https://api.wikimedia.org/feed/v1/wikipedia/en/featured/${replacedDate}`;
+  renderBanner(url);
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const today = new Date();
+  const year = today.getFullYear();
+  const month = String(today.getMonth() + 1).padStart(2, "0");
+  const day = String(today.getDate()).padStart(2, "0");
+  const formattedDate = `${year}/${month}/${day}`;
+  const url = `https://api.wikimedia.org/feed/v1/wikipedia/en/featured/${formattedDate}`;
+  renderBanner(url);
+  $input.value = `${year}-${month}-${day}`;
+});
